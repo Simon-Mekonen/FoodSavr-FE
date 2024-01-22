10192460893e5d5dbfbcff479260c23f138dcbd4
Simon Mekonen <simon.mekonen@b3.se>
2024-01-22 15:01:08 +0100

    updated component with types

--- src/components/recipeCard.tsx
+++ src/components/recipeCard.tsx
@@ -1,16 +1,23 @@
 import * as React from "react";
 import { Box } from "@mui/system";
+import { IRecipeComplete } from "../API/API.types";
 
-export default function RecipeCard({ recipeCardData, key }) {
+interface RecipeCardProps {
+  recipeCardData: IRecipeComplete;
+  key: string | number;
+}
+const RecipeCard: React.FC<RecipeCardProps> = ({ recipeCardData, key }) => {
   return (
     <Box sx={boxSize} key={key} onClick={() => console.log(recipeCardData)}>
       {/* Add Cardomponent.js from old repo */}
     </Box>
   );
-}
+};
 
 const boxSize = {
   width: "356px",
   mt: "20px",
   mx: "10px",
 };
+
+export default RecipeCard;

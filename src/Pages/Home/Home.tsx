0c21a00140d8746e9b91435090e7a09061d233ff
Simon Mekonen <simon.mekonen@b3.se>
2024-02-19 15:20:30 +0100

    moved testdata to Home.tsx

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,21 @@
+import { testDataIngredients, testDataRecipeMatches } from "../../../testData";
 import { RecipeCardList } from "../../components/RecipeCardList/recipeCardList";
 import { SearchBar } from "../../components/Searchbar/searchbar";
 import { StyledH1 } from "../Recipe/recipe.styles";
 
 const Home = () => {
+  // Test data
+  const recipeCardDataList = testDataRecipeMatches;
+  const ingredientCount = testDataIngredients.length;
+
   return (
     <>
       <StyledH1>FoodSavr</StyledH1>
       <SearchBar />
-      <RecipeCardList />
+      <RecipeCardList
+        recipeCardDataList={recipeCardDataList}
+        ingredientCount={ingredientCount}
+      />
     </>
   );
 };
--- src/components/RecipeCardList/recipeCardList.tsx
+++ src/components/RecipeCardList/recipeCardList.tsx
@@ -1,4 +1,3 @@
-import { testDataIngredients, testDataRecipeMatches } from "../../../testData";
 import { RecipeCard } from "../RecipeCard/recipeCard";
 import { IRecipeCardListProps } from "./recipeCardList.types";
 import { StyledBox } from "./recipeCardList.styles";
@@ -7,10 +6,6 @@ export const RecipeCardList: React.FC<IRecipeCardListProps> = ({
   recipeCardDataList,
   ingredientCount,
 }) => {
-  // Test data
-  recipeCardDataList = testDataRecipeMatches;
-  ingredientCount = testDataIngredients.length;
-
   return (
     recipeCardDataList && (
       <StyledBox>

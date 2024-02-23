171e5188fa0a69ff861926fe59049ba431563164
Simon Mekonen <simon.mekonen@b3.se>
2024-02-23 08:44:36 +0100

    added states for ingredients and recipe

--- src/store/recoilStore.tsx
+++ src/store/recoilStore.tsx
@@ -1,6 +1,6 @@
 import { atom, selector } from "recoil";
-import { testDataIngredients } from "../../testData";
-import { IIngredient } from "../API/API.types";
+import { testDataIngredients, testDataRecipe } from "../../testData";
+import { IIngredient, IRecipeComplete } from "../API/API.types";
 
 export const ingredientOptionsState = atom<IIngredient[]>({
   key: "ingredientOptionsState",
@@ -13,10 +13,15 @@ export const ingredientState = atom<IIngredient[]>({
 });
 
 export const ingredientIdState = selector<number[]>({
-  key: "ingredientIdsState",
+  key: "ingredientIdState",
   get: ({ get }) => {
     const ingredients = get(ingredientState);
     const ids = ingredients.map((ingredient) => ingredient.id);
     return ids;
   },
+});
+
+export const recipeState = atom<IRecipeComplete>({
+  key: "recipeState",
+  default: testDataRecipe as IRecipeComplete,
 });

2bc98ba4b8a127b50799276a03568884c7de1e11
Simon Mekonen <simon.mekonen@b3.se>
2024-02-27 10:10:58 +0100

    added return of just ingredients in case the conversion fails

--- src/utils/converterUtils.tsx
+++ src/utils/converterUtils.tsx
@@ -6,21 +6,26 @@ export const addIngredientReplacements = (
   converter: IIngredientConverter[]
 ): IRecipeIngredientComplete[] => {
   const updatedIngredients: IRecipeIngredientComplete[] = [...ingredients];
-  // Loop through each conversion in the converter array
-  converter.forEach((converterItem) => {
-    // Loop through each ingredient in the ingredients array
-    updatedIngredients.forEach((ingredientItem) => {
-      // Check if the original ingredient in the conversion matches the current ingredient
-      // and if there is a replacement specified in the conversion
-      if (
-        converterItem.originalIngredient?.toLocaleLowerCase() ===
-          ingredientItem.originalIngredient?.toLocaleLowerCase() &&
-        converterItem.replacementIngredient?.toLocaleLowerCase()
-      ) {
-        // Update the current ingredient's replacement with the one specified in the conversion
-        ingredientItem.replacement = converterItem.replacementIngredient;
-      }
+  try {
+    // Loop through each conversion in the converter array
+    converter.forEach((converterItem) => {
+      // Loop through each ingredient in the ingredients array
+      updatedIngredients.forEach((ingredientItem) => {
+        // Check if the original ingredient in the conversion matches the current ingredient
+        // and if there is a replacement specified in the conversion
+        if (
+          converterItem.originalIngredient?.toLocaleLowerCase() ===
+            ingredientItem.originalIngredient?.toLocaleLowerCase() &&
+          converterItem.replacementIngredient?.toLocaleLowerCase()
+        ) {
+          // Update the current ingredient's replacement with the one specified in the conversion
+          ingredientItem.replacement = converterItem.replacementIngredient;
+        }
+      });
     });
-  });
-  return updatedIngredients;
+    return updatedIngredients;
+  } catch (error) {
+    console.error("Error at appending replacements: ", error);
+    return ingredients;
+  }
 };

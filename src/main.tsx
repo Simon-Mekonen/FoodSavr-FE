250937570cd6d84a0abc52307eab0cd93aaf6372
Simon Mekonen <simon.mekonen@b3.se>
2024-03-05 13:13:01 +0100

    added debug of recoil state

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import RecipePage from "./Pages/Recipe/Recipe";
 import { RecoilRoot } from "recoil";
 import { GlobalStyle } from "./styles/theme";
 import Home from "./Pages/Home/Home";
+import { DebugObserver } from "./store/debug";
 // import HomeContainer from "./Pages/Home/HomeContainer";
 
 export const router = createBrowserRouter([
@@ -16,7 +17,15 @@ export const router = createBrowserRouter([
     errorElement: <ErrorPage />,
   },
   {
-    path: "/recipe",
+    path: "recipe",
+    element: <RecipePage />,
+    errorElement: <ErrorPage />,
+    loader: async () => {
+      console.log("LOADING");
+    },
+  },
+  {
+    path: "recipe/:recipeId/:ingredientIds",
     element: <RecipePage />,
     errorElement: <ErrorPage />,
   },
@@ -25,6 +34,7 @@ export const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RecoilRoot>
+      <DebugObserver />
       <GlobalStyle /> {/* Include GlobalStyle within React.StrictMode */}
       <RouterProvider router={router} />
     </RecoilRoot>
